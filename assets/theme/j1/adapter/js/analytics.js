58d162fed8b2ebba40bcb9e8d35f6a40f23bdf69
leleulabs <leleulabsllc@gmail.com>
2025-09-26 18:07:27 -1000

    Removed cookie consent form

--- assets/theme/j1/adapter/js/analytics.js
+++ assets/theme/j1/adapter/js/analytics.js
@@ -11,7 +11,7 @@
  # J1 Template is licensed under the MIT License.
  # For details, see: https://github.com/jekyll-one-org/j1-template/blob/main/LICENSE
  # -----------------------------------------------------------------------------
- #  Adapter generated: 2025-09-27 02:24:16 +0200
+ #  Adapter generated: 2025-09-27 03:33:52 +0200
  # -----------------------------------------------------------------------------
 */
 "use strict";j1.adapter.analytics=((e,t)=>{"development"===e.env||e.env;var a,n,i;new liteURL(t.location.href).hostname,document.createElement('script'),e.getCookieNames(),(new Date).toISOString();return{init:()=>{var t=setInterval(()=>{var a='block'===$('#content').css("display");'finished'===e.getState()&&a&&((n=log4javascript.getLogger('j1.adapter.analytics')).info('Google Analytics: disabled'),clearInterval(t))},10)},messageHandler:(e,t)=>{var a=JSON.stringify(t,undefined,2);return i='received message from '+e+': '+a,n.debug(i),'command'===t.type&&'module_initialized'===t.action&&n.info(t.text),!0},setState:e=>{a.state=e},getState:()=>a.state}})(j1,window);
